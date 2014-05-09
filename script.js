e5c06e095980d5b1f3d6dcfe3b31783ca49d8365
Thomas Giles <wthit56@googlemail.com>
2014-05-09 15:23:09 +0100

    Options
    Option Saving, Loading, Auto, Submit

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ window.addEventListener("load", function () {
 		var amount, boxes, window;
 		function scroll(source) {
 			scroll.allow = false;
-			console.log(source);
 
 			if (source.nodeType === 1) {
 				amount = source.scrollTop / (source.scrollHeight - source.offsetHeight);
@@ -57,6 +56,154 @@ window.addEventListener("load", function () {
 	outputPreview.contentWindow.onscroll = scroll.event;
 	outputPreview.contentDocument.body.style.margin = "10px";
 	scroll.boxes.push(outputPreview.contentWindow);
+
+	var load = (function () {
+		var cookie, from, to;
+		return function load(field) {
+			cookie = document.cookie;
+
+			from = cookie.indexOf(field.id + "=");
+			if (from === -1) { return; }
+			from += field.id.length + 1;
+
+			to = cookie.indexOf(";", from);
+			if (to === -1) { to = cookie.length; }
+
+			if (field.type === "checkbox") {
+				field.checked = (cookie.substring(from, to) !== "false");
+			}
+			else {
+				field.value = cookie.substring(from, to);
+			}
+
+			save(field);
+			cookie = null;
+		}
+	})();
+	function save(field) {
+		document.cookie = (
+			field.id + "=" + ((field.type === "checkbox") ? field.checked : field.value) + ";" +
+			"expires=" + new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toGMTString()
+		);
+	}
+
+	var markup = {
+		"para-before": "\n<p>", "para-after": "</p>\n",
+		"bold-before": "<strong>", "bold-after": "</strong>",
+		"underlined-before": "<u>", "underlined-after": "</u>",
+		"italic-before": "<em>", "italic-after": "</em>",
+		"break": "\n<hr />\n",
+		fromInput: (function () {
+			var findToEscape = /"|\\(?!['"\\ntbfu])/g;
+			return function (input) {
+				return (this[input.id] = JSON.parse("\"" + input.value.replace(findToEscape, "\\$&") + "\""));
+			};
+		})()
+	};
+	function render() {
+		output.value = content.map(function (part) {
+			switch (part) {
+				case breakObject:
+					return markup["break"];
+				default:
+					return markup["para-before"] + part.map(function (text) {
+						return (
+							(text.B ? markup["bold-before"] : "") +
+							(text.U ? markup["underlined-before"] : "") +
+							(text.I ? markup["italic-before"] : "") +
+							text.text +
+							(text.I ? markup["italic-after"] : "") +
+							(text.U ? markup["underlined-after"] : "") +
+							(text.B ? markup["bold-after"] : "")
+						);
+					}).join("") + markup["para-after"];
+			}
+		}).join("");
+	}
+
+	var output = (function () {
+		var output = document.getElementById("output");
+		output.onscroll = scroll.event;
+		scroll.boxes.push(output);
+
+		var month = 30 * 24 * 60 * 60 * 1000;
+		var maxWidth;
+		var controls = output.controls = (function () {
+			var controls = document.getElementById("output-controls");
+			var height, h;
+
+			var auto = {
+				save: document.getElementById("output-save-auto"),
+				render: document.getElementById("output-render-auto")
+			};
+			load(auto.save); load(auto.render);
+			auto.save.onchange = auto.render.onchange = function (e) {
+				save(e.target || e.srcElement);
+			};
+
+			var proxy = document.getElementById("option-proxy"), newWidth;
+
+			function resizeOption(option) {
+				proxy.style.display = "inline-block";
+				proxy.innerText = option.value;
+				option.style.width = proxy.offsetWidth + "px";
+				proxy.style.display = "none";
+				if ((h = controls.offsetHeight) !== height) {
+					resize.outputHeight();
+					height = h;
+				}
+			}
+
+			function change(option) {
+				if (option.value === option.oldValue) { return; }
+
+				resizeOption(option);
+
+				markup.fromInput(option);
+				if (auto.save.checked) { save(option); }
+
+				option.oldValue = option.value;
+			}
+			change.debounce = (function () {
+				var scheduled = null, options = [];
+				function doChange() {
+					var option;
+					while (option = options.pop()) {
+						change(option);
+					}
+					if (auto.render.checked) { render(); }
+
+					scheduled = null;
+				}
+				return function (option) {
+					if (options.indexOf(option) === -1) { options.push(option); }
+					if (scheduled === null) {
+						scheduled = setTimeout(doChange, 0);
+					}
+				}
+			})();
+			change.event = function changeEvent(e) {
+				change.debounce(e.target || e.srcElement);
+			};
+
+			var options = controls.options = document.getElementById("output-controls-markup").getElementsByTagName("INPUT");
+			for (var i = 0, l = options.length; i < l; i++) {
+				load(options[i]);
+				options[i].oldValue = options[i].value;
+				markup.fromInput(options[i]);
+
+				proxy.innerText = options[i].value;
+				options[i].style.width = proxy.offsetWidth + "px";
+				options[i].onchange = options[i].onkeyup = options[i].onkeydown = change.event;
+			}
+			proxy.style.display = "none";
+			height = controls.offsetHeight;
+
+			return controls;
+		})();
+
+		return output;
+	})();
 
 	var content = [], breakObject = { is: "break" };
 	document.getElementById("input-analyse").onclick = (function () {
@@ -167,23 +314,6 @@ window.addEventListener("load", function () {
 			};
 		})();
 
-		var paraBefore = document.getElementById("para-before"),
-			paraAfter = document.getElementById("para-after"),
-			boldBefore = document.getElementById("bold-before"),
-			boldAfter = document.getElementById("bold-after"),
-			underlinedBefore = document.getElementById("underlined-before"),
-			underlinedAfter = document.getElementById("underlined-after"),
-			italicBefore = document.getElementById("italic-before"),
-			italicAfter = document.getElementById("italic-after"),
-			breakOption = document.getElementById("break");
-
-		var toString = (function () {
-			var findQuotes = /"/g;
-			return function toString(string) {
-				return JSON.parse("\"" + string.replace(findQuotes, "\\\"") + "\"");
-			};
-		})();
-
 		return function () {
 			content.length = 0;
 			parse(input.contentDocument.body);
@@ -209,24 +339,7 @@ window.addEventListener("load", function () {
 				}
 			}).join("\n");
 
-			output.value = content.map(function (part) {
-				switch (part) {
-					case breakObject:
-						return toString(breakOption.value);
-					default:
-						return toString(paraBefore.value) + part.map(function (text) {
-							return (
-								(text.B ? toString(boldBefore.value) : "") +
-								(text.U ? toString(underlinedBefore.value) : "") +
-								(text.I ? toString(italicBefore.value) : "") +
-								text.text +
-								(text.I ? toString(italicAfter.value) : "") +
-								(text.U ? toString(underlinedAfter.value) : "") +
-								(text.B ? toString(boldAfter.value) : "")
-							);
-						}).join("") + toString(paraAfter.value);
-				}
-			}).join("");
+			render();
 
 			scroll(outputPreview.contentDocument);
 		};
@@ -237,63 +350,6 @@ window.addEventListener("load", function () {
 		outputPreview.contentDocument.body.innerHTML = "";
 		output.value = "";
 	};
-
-	var output = (function () {
-		var output = document.getElementById("output");
-		output.onscroll = scroll.event;
-		scroll.boxes.push(output);
-
-		var maxWidth;
-		var controls = output.controls = (function () {
-			var controls = document.getElementById("output-controls");
-			var height, h;
-
-			var proxy = document.getElementById("option-proxy"), newWidth;
-			function change(option) {
-				proxy.style.display = "inline-block";
-				proxy.innerText = option.value;
-				option.style.width = proxy.offsetWidth + "px";
-				proxy.style.display = "none";
-				if ((h = controls.offsetHeight) !== height) {
-					resize.outputHeight();
-					height = h;
-				}
-			}
-			change.debounce = (function () {
-				var scheduled = null, options = [];
-				function doChange() {
-					var option;
-					while (option = options.pop()) {
-						change(option);
-					}
-					scheduled = null;
-				}
-				return function (option) {
-					if (options.indexOf(option) === -1) { options.push(option); }
-					if (scheduled === null) {
-						scheduled = setTimeout(doChange, 0);
-					}
-				}
-			})();
-			change.event = function changeEvent(e) {
-				change.debounce(e.target || e.srcElement);
-			};
-
-			var options = controls.options = document.getElementById("output-controls-markup").getElementsByTagName("INPUT");
-			for (var i = 0, l = options.length; i < l; i++) {
-				proxy.innerText = options[i].value;
-				options[i].style.width = proxy.offsetWidth + "px";
-				options[i].onchange = options[i].onkeyup = options[i].onkeydown = change.event;
-			}
-			proxy.style.display = "none";
-
-			height = controls.offsetHeight;
-
-			return controls;
-		})();
-
-		return output;
-	})();
 
 	var scrollbarWidth = (function () {
 		var div = document.createElement("DIV");
@@ -366,6 +422,18 @@ window.addEventListener("load", function () {
 		return { outputHeight: resizeOutputHeight };
 	})();
 
+	document.getElementById("output-save").addEventListener("click", function (e) {
+		var options = output.controls.options;
+		for (var i = 0, l = options.length; i < l; i++) {
+			save(options[i]);
+		}
+	});
+
+	document.getElementById("output-render").addEventListener("click", output.controls.onsubmit= function (e) {
+		e.preventDefault();
+		render(); scroll(output);
+		return false;
+	});
 });
 /*
 var input = document.getElementById("input"),
